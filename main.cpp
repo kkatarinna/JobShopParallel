75c94c1c2046531a96b19995733688004d760a7d
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2023-06-05 14:58:57 +0200

    using tbb

--- main.cpp
+++ main.cpp
@@ -6,25 +6,7 @@
 #include "constants.h"
 
 int main() {
-
-
-//    for (const auto& pair : JOBS) {
-//        std::cout << pair.first << "::" << pair.second << "\n";
-//    }
-//
-//    for (const auto& pair : JOBS_KEYS){
-//        cout<<pair<<endl;
-//    }
-
-//    for(const auto& pair :MACHINES){
-//        cout<<pair.first<<endl;
-//        for(const auto& inner_dict : pair.second){
-//            cout<<inner_dict.first<<endl;
-//            for (const auto& inner_inner_vect :inner_dict.second){
-//                cout<<inner_inner_vect<<endl;
-//            }
-//        }
-//    }
+    
     std::vector<std::vector<std::string>> chromosome;
     Chromosome proba(JOBS, MACHINES, chromosome);
     cout<<proba.to_string()<<endl;
