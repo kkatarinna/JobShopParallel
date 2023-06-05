5684e429c5bb7903e2559e908e81d4c32738387f
Katarina Krstin <92788177+kkatarinna@users.noreply.github.com>
2023-06-05 14:44:33 +0200

    serijski

--- geneticAlgorithm.cpp
+++ geneticAlgorithm.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "geneticAlgorithm.h"
+using namespace std;
+
+vector<Chromosome> genetic(int population_size, double mutation_rate, int max_iter, double convergence_check){
+    vector<Chromosome> chromosomes = generate_chromosome_list(population_size, JOBS, MACHINES, {});
+    vector<Chromosome> the_best_list;
+    vector<int> avg_values;
+    vector<int> list_of_averages_trough_time;
+    vector<int> list_of_best_values_trough_time;
+
+    for(int i=0; i<max_iter; i++){
+        std::map<Chromosome, int> chromosome_dict = get_chromosome_rang_dict(chromosomes, JOBS, MACHINES);
+        std::vector<std::pair <Chromosome, Chromosome>> pairs = rulette_selection(chromosome_dict);
+        std::vector<Chromosome> newgen = crossover(pairs);
+        std::vector<Chromosome> mutirani = mutation(newgen, 0.7);
+
+        vector<vector<vector<string>>> hromozomiZaProsledjivanje;
+        for(auto& chromosome : chromosomes){
+            hromozomiZaProsledjivanje.push_back(chromosome.chromosome);
+        }
+        chromosomes = generate_chromosome_list(population_size, JOBS, MACHINES, hromozomiZaProsledjivanje);
+        cout<<"NEW GEN "+ std::to_string(i+1) +" =============================================="<<endl;
+        for(auto& ch : chromosomes){
+            cout<<ch.to_string()<<endl;
+        }
+    }
+
+    return chromosomes;
+}
--- geneticAlgorithm.h
+++ geneticAlgorithm.h
@@ -4,5 +4,11 @@
 
 #ifndef PROJEKAT_GENETICALGORITHM_H
 #define PROJEKAT_GENETICALGORITHM_H
+#include <iostream>
+#include "Chromosome.h"
+#include "constants.h"
+#include "mutation.h"
+#include "crossoverFunc.h"
 
+vector<Chromosome> genetic(int population_size, double mutation_rate = 0.7, int max_iter = 100, double convergence_check = 0.005);
 #endif //PROJEKAT_GENETICALGORITHM_H
